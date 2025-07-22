673b83822d414dc3e67dad2c36326e0eb14f846c
Oussama <earsvision1@gmail.com>
2025-07-22 23:17:58 +0200

    Update Dotnet bindings

--- SpeechCore.CrossPlatform/SpeechCore.cs
+++ SpeechCore.CrossPlatform/SpeechCore.cs
@@ -15,6 +15,7 @@ public class SpeechCore : IDisposable
     public const uint SC_HAS_SPEECH = 1 << 4;
     public const uint SC_HAS_BRAILLE = 1 << 5;
     public const uint SC_HAS_SPEECH_STATE = 1 << 6;
+    public const uint SC_SSML_SUPPORT = 1 << 7;
 
     private static readonly bool IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
 
@@ -58,6 +59,14 @@ public class SpeechCore : IDisposable
     [return: MarshalAs(UnmanagedType.Bool)]
     private static extern bool Speech_Output_Unix(IntPtr textPtr, [MarshalAs(UnmanagedType.Bool)] bool interrupt);
 
+    [DllImport(DllName, EntryPoint = "Speech_Output_text")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool Speech_Output_text_Windows([MarshalAs(UnmanagedType.LPWStr)] string text, [MarshalAs(UnmanagedType.Bool)] bool interrupt, [MarshalAs(UnmanagedType.Bool)] bool with_ssml);
+
+    [DllImport(DllName, EntryPoint = "Speech_Output_text")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool Speech_Output_text_Unix(IntPtr textPtr, [MarshalAs(UnmanagedType.Bool)] bool interrupt, [MarshalAs(UnmanagedType.Bool)] bool with_ssml);
+
     [DllImport(DllName, EntryPoint = "Speech_Braille")]
     [return: MarshalAs(UnmanagedType.Bool)]
     private static extern bool Speech_Braille_Windows([MarshalAs(UnmanagedType.LPWStr)] string text);
@@ -81,6 +90,12 @@ public class SpeechCore : IDisposable
 
     [DllImport(DllName)]
     private static extern void Speech_Set_Rate(float offset);
+
+    [DllImport(DllName)]
+    private static extern float Speech_Get_Pitch();
+
+    [DllImport(DllName)]
+    private static extern void Speech_Set_Pitch(float offset);
 
     [DllImport(DllName)]
     private static extern IntPtr Speech_Get_Current_Voice();
@@ -108,6 +123,67 @@ public class SpeechCore : IDisposable
 
     [DllImport(DllName)]
     private static extern void Speech_Prefer_Sapi([MarshalAs(UnmanagedType.Bool)] bool prefer_sapi);
+
+    [DllImport(DllName)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool Speech_Sapi_Loaded();
+
+    [DllImport(DllName)]
+    private static extern void Sapi_Init();
+
+    [DllImport(DllName)]
+    private static extern void Sapi_Release();
+
+    [DllImport(DllName)]
+    private static extern IntPtr Sapi_Get_Current_Voice();
+
+    [DllImport(DllName)]
+    private static extern IntPtr Sapi_Get_Voice(int index);
+
+    [DllImport(DllName, EntryPoint = "Sapi_Set_Voice")]
+    private static extern void Sapi_Set_Voice_Windows([MarshalAs(UnmanagedType.LPWStr)] string voice);
+
+    [DllImport(DllName, EntryPoint = "Sapi_Set_Voice")]
+    private static extern void Sapi_Set_Voice_Unix(IntPtr voicePtr);
+
+    [DllImport(DllName)]
+    private static extern void Sapi_Set_Voice_By_Index(int index);
+
+    [DllImport(DllName)]
+    private static extern int Sapi_Get_Voices();
+
+    [DllImport(DllName)]
+    private static extern float Sapi_Voice_Get_Volume();
+
+    [DllImport(DllName)]
+    private static extern void Sapi_Voice_Set_Volume(float volume);
+
+    [DllImport(DllName)]
+    private static extern float Sapi_Voice_Get_Rate();
+
+    [DllImport(DllName)]
+    private static extern void Sapi_Voice_Set_Rate(float rate);
+
+    [DllImport(DllName, EntryPoint = "Sapi_Speak")]
+    private static extern void Sapi_Speak_Windows([MarshalAs(UnmanagedType.LPWStr)] string text, [MarshalAs(UnmanagedType.Bool)] bool interrupt, [MarshalAs(UnmanagedType.Bool)] bool xml);
+
+    [DllImport(DllName, EntryPoint = "Sapi_Speak")]
+    private static extern void Sapi_Speak_Unix(IntPtr textPtr, [MarshalAs(UnmanagedType.Bool)] bool interrupt, [MarshalAs(UnmanagedType.Bool)] bool xml);
+
+    [DllImport(DllName, EntryPoint = "Sapi_Output_File")]
+    private static extern void Sapi_Output_File_Windows([MarshalAs(UnmanagedType.LPStr)] string filename, [MarshalAs(UnmanagedType.LPWStr)] string text, [MarshalAs(UnmanagedType.Bool)] bool xml);
+
+    [DllImport(DllName, EntryPoint = "Sapi_Output_File")]
+    private static extern void Sapi_Output_File_Unix([MarshalAs(UnmanagedType.LPStr)] string filename, IntPtr textPtr, [MarshalAs(UnmanagedType.Bool)] bool xml);
+
+    [DllImport(DllName)]
+    private static extern void Sapi_Pause();
+
+    [DllImport(DllName)]
+    private static extern void Sapi_Resume();
+
+    [DllImport(DllName)]
+    private static extern void Sapi_Stop();
 
     public SpeechCore()
     {
@@ -229,6 +305,30 @@ public class SpeechCore : IDisposable
         }
     }
 
+    public bool SpeakWithSSML(string text, bool interrupt = false, bool with_ssml = false)
+    {
+        if (string.IsNullOrEmpty(text))
+            return false;
+
+        if (IsWindows)
+        {
+            return Speech_Output_text_Windows(text, interrupt, with_ssml);
+        }
+        else
+        {
+            IntPtr textPtr = StringToWChar(text);
+            try
+            {
+                return Speech_Output_text_Unix(textPtr, interrupt, with_ssml);
+            }
+            finally
+            {
+                if (textPtr != IntPtr.Zero)
+                    Marshal.FreeHGlobal(textPtr);
+            }
+        }
+    }
+
     public bool Braille(string text)
     {
         if (string.IsNullOrEmpty(text))
@@ -262,6 +362,10 @@ public class SpeechCore : IDisposable
     public float GetRate() => Speech_Get_Rate();
 
     public void SetRate(float rate) => Speech_Set_Rate(rate);
+
+    public float GetPitch() => Speech_Get_Pitch();
+
+    public void SetPitch(float pitch) => Speech_Set_Pitch(pitch);
 
     public string GetCurrentVoice() => WCharToString(Speech_Get_Current_Voice());
 
@@ -299,4 +403,95 @@ public class SpeechCore : IDisposable
     public bool CheckSpeechFlags(uint flags) => (GetSpeechFlags() & flags) != 0;
 
     public void PreferSapi(bool preferSapi) => Speech_Prefer_Sapi(preferSapi);
+
+    public bool SapiLoaded() => Speech_Sapi_Loaded();
+
+    public void SapiInit() => Sapi_Init();
+
+    public void SapiRelease() => Sapi_Release();
+
+    public string SapiGetCurrentVoice() => WCharToString(Sapi_Get_Current_Voice());
+
+    public string SapiGetVoice(int index) => WCharToString(Sapi_Get_Voice(index));
+
+    public void SapiSetVoice(string voice)
+    {
+        if (IsWindows)
+        {
+            Sapi_Set_Voice_Windows(voice);
+        }
+        else
+        {
+            IntPtr voicePtr = StringToWChar(voice);
+            try
+            {
+                Sapi_Set_Voice_Unix(voicePtr);
+            }
+            finally
+            {
+                if (voicePtr != IntPtr.Zero)
+                    Marshal.FreeHGlobal(voicePtr);
+            }
+        }
+    }
+
+    public void SapiSetVoiceByIndex(int index) => Sapi_Set_Voice_By_Index(index);
+
+    public int SapiGetVoices() => Sapi_Get_Voices();
+
+    public float SapiVoiceGetVolume() => Sapi_Voice_Get_Volume();
+
+    public void SapiVoiceSetVolume(float volume) => Sapi_Voice_Set_Volume(volume);
+
+    public float SapiVoiceGetRate() => Sapi_Voice_Get_Rate();
+
+    public void SapiVoiceSetRate(float rate) => Sapi_Voice_Set_Rate(rate);
+
+    public void SapiSpeak(string text, bool interrupt = false, bool xml = false)
+    {
+        if (IsWindows)
+        {
+            Sapi_Speak_Windows(text, interrupt, xml);
+        }
+        else
+        {
+            IntPtr textPtr = StringToWChar(text);
+            try
+            {
+                Sapi_Speak_Unix(textPtr, interrupt, xml);
+            }
+            finally
+            {
+                if (textPtr != IntPtr.Zero)
+                    Marshal.FreeHGlobal(textPtr);
+            }
+        }
+    }
+
+    public void SapiOutputFile(string filename, string text, bool xml = false)
+    {
+        if (IsWindows)
+        {
+            Sapi_Output_File_Windows(filename, text, xml);
+        }
+        else
+        {
+            IntPtr textPtr = StringToWChar(text);
+            try
+            {
+                Sapi_Output_File_Unix(filename, textPtr, xml);
+            }
+            finally
+            {
+                if (textPtr != IntPtr.Zero)
+                    Marshal.FreeHGlobal(textPtr);
+            }
+        }
+    }
+
+    public void SapiPause() => Sapi_Pause();
+
+    public void SapiResume() => Sapi_Resume();
+
+    public void SapiStop() => Sapi_Stop();
 }
